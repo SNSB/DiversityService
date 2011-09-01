dcfc9e8fa9eec6cf181531766a23f5e43bd0a795
Georg Rollinger <theotherspriest@web.de>
2011-09-01 10:51:40 +0200

    Taxon Groups Download

---
+++ DiversityService.Model/TermLists.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DiversityService.Model;
+
+namespace DiversityService
+{
+    public enum TermLists
+    {
+        TaxonomicGroups = 0,      
+    }
+}
